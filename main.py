8ed003a11662578f6854d7fbf57da77df869665e
Aryan Yadav <ay6033756@gmail.com>
2026-06-21 21:20:32 +0530

    adding data code

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
 from fastapi import FastAPI
+import json
 
 app = FastAPI()
+
+def load_config():
+    with open("patients.json") as config_file:
+        config = json.load(config_file)
+    return config
 
 @app.get("/")
 def read_root():
@@ -10,3 +16,8 @@ def read_root():
 @app.get("/about")
 def about():
     return {"About": "This is a FastAPI application."} 
+
+@app.get("/view")
+def view_patients():
+    data = load_config()
+    return data

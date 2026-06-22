757f602c2fbf4b2c6fbff87cd148a850bc8aab44
Aryan Yadav <ay6033756@gmail.com>
2026-06-23 01:36:13 +0530

    adding path method

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI
+from fastapi import FastAPI , Path , HTTPException , Query
 import json
 
 app = FastAPI()
@@ -21,3 +21,24 @@ def about():
 def view_patients():
     data = load_config()
     return data
+
+
+@app.get("/patients/{patient_id}")
+def view_patient(patient_id: str = Path(..., description="ID of the patient in the DB", example="P001")):
+    # load all data of the patients
+    data = load_config()
+    # check if the patient id exists in the data
+    if patient_id in data:
+        return data[patient_id]
+    else:
+        raise HTTPException(status_code=404, detail="Patient not found")
+    
+    
+@app.get("/sort")
+def sort_patients(sort_by : str = Query(..., description="Sort on the basis of height , weight or bmi "), order: str = Query("asc", description="Order of sorting: asc or desc")):
+    data = load_config()
+    if sort_by not in ["height", "weight", "bmi"]:
+        raise HTTPException(status_code=400, detail="Invalid sort_by value. Must be 'height', 'weight', or 'bmi'.")
+    
+    sorted_data = sorted(data.items(), key=lambda x: x[1][sort_by], reverse=(order == "desc"))
+    return {patient_id: details for patient_id, details in sorted_data}

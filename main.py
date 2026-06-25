9bf5eb3f371c53965031ff441b81665509919828
Aryan Yadav <ay6033756@gmail.com>
2026-06-26 00:14:09 +0530

    updating edit feature

--- main.py
+++ main.py
@@ -38,6 +38,15 @@ class Patient(BaseModel):
         
 
 
+class PatientUpdate(BaseModel):
+    name: Annotated[Optional[str], Field(default=None)]
+    city: Annotated[Optional[str], Field(default=None)]
+    age: Annotated[Optional[int], Field(default=None, gt=0)]
+    gender: Annotated[Optional[Literal['male', 'female']], Field(default=None)]
+    height: Annotated[Optional[float], Field(default=None, gt=0)]
+    weight: Annotated[Optional[float], Field(default=None, gt=0)]
+
+
 def load_config():
     with open("patients.json") as config_file:
         config = json.load(config_file)
@@ -56,6 +65,7 @@ def read_root():
 @app.get("/about")
 def about():
     return {"About": "This is a FastAPI application."} 
+
 
 @app.get("/view")
 def view_patients():
@@ -119,3 +129,35 @@ def create_patient(patient: Patient):
     save_data(data)
     
     return JSONResponse(status_code=201, content={'message':'patient created successfully'})
+
+
+
+
+@app.put('/edit/{patient_id}')
+def update_patient(patient_id: str, patient_update: PatientUpdate):
+
+    data = load_data()
+
+    if patient_id not in data:
+        raise HTTPException(status_code=404, detail='Patient not found')
+    
+    existing_patient_info = data[patient_id]
+
+    updated_patient_info = patient_update.model_dump(exclude_unset=True)
+
+    for key, value in updated_patient_info.items():
+        existing_patient_info[key] = value
+
+    #existing_patient_info -> pydantic object -> updated bmi + verdict
+    existing_patient_info['id'] = patient_id
+    patient_pydandic_obj = Patient(**existing_patient_info)
+    #-> pydantic object -> dict
+    existing_patient_info = patient_pydandic_obj.model_dump(exclude='id')
+
+    # add this dict to data
+    data[patient_id] = existing_patient_info
+
+    # save data
+    save_data(data)
+
+    return JSONResponse(status_code=200, content={'message':'patient updated'})
